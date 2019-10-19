1714de9706b08390893cc09a28c7f126f5ec4dad
Roman Shveda <>
2019-10-19 15:43:11 +0300

    Initial project setup

---
+++ MYWEATHER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYWEATHER
+//
+//  Created by Roman Shveda on 10/19/19.
+//  Copyright © 2019 Roman Shveda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
